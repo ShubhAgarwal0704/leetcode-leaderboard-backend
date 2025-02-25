cd828145c96bc28f9d0cea531c8d808616581547
Shubh Agarwal <shubhagarwal0704@gmail.com>
2025-02-25 15:35:38 +0530

    minnor change

--- src/main/java/com/project/leetcodedashboard/controllers/UserController.java
+++ src/main/java/com/project/leetcodedashboard/controllers/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "api/v0")
+@RequestMapping("api/v0")
 public class UserController {
     @Autowired
     private UserService userService;
